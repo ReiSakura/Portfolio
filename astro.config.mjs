ee8ebc386707ea73ea0424f40a69bec59b72b01d
ReiSakura <anninfinity21@gmail.com>
2025-04-16 16:58:33 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://reisakura.github.io",
+    site: "https://Amanda.github.io",
     integrations: [mdx(), sitemap(), tailwind()],
 });
